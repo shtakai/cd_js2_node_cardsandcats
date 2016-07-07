569192bed5f04dd780e7477d96ebcb1122b8f2ff
Alyson Takai <shtakai@gmail.com>
2016-07-07 10:44:41 -0700

    modified

--- app.js
+++ app.js
@@ -3,10 +3,24 @@ var http,
     port
 http = require('http');
 fs = require('fs');
-port = 6789;
+port = 7077;
 
 var server = http.createServer(function(request, response){
   console.log(`client request url: ${request.url}`);
+
+  if(request.url === '/cars'){
+
+    // /cars =>
+    //  simple html page with few car image in image folder
+    fs.readFile('views/car.html', 'utf8', function(error, contents){
+      response.writeHead(200, {'Content-Type': 'text/html'});
+      response.write(contents);
+      response.end();
+    });
+  }else{
+    response.writeHead(404);
+    response.end(`not found :O ${request.url}`);
+  }
 
 
 
