5e7faeb311974cfccc8c14907d7f4ad20b0485de
Alyson Takai <shtakai@gmail.com>
2016-07-07 12:24:58 -0700

    modified

--- app.js
+++ app.js
@@ -16,6 +16,46 @@ var server = http.createServer(function(request, response){
       response.writeHead(200, {'Content-Type': 'text/html'});
       response.write(contents);
       response.end();
+    console.log('-------');
+    });
+  }else if(request.url === '/stylesheet/style.css'){
+    fs.readFile('stylesheets/style.css', 'utf8', function(error, contents){
+      response.writeHead(200, {'Content-Type': 'text/css'});
+      response.write(contents);
+      response.end();
+    });
+  }else if(request.url ==='/images/car1.png'){
+    fs.readFile('images/car1.png', function(error, contents){
+      response.writeHead(200, {'Content-Type': 'image/*'});
+      response.write(contents);
+      response.end();
+    });
+  }else if(request.url ==='/images/car2.png'){
+    fs.readFile('images/car2.png', function(error, contents){
+      response.writeHead(200, {'Content-Type': 'image/*'});
+      response.write(contents);
+      response.end();
+    });
+  } else if(request.url === '/cats'){
+
+    // /cats =>
+    //  simple html page with few cat image in image folder
+    fs.readFile('views/cat.html', 'utf8', function(error, contents){
+      response.writeHead(200, {'Content-Type': 'text/html'});
+      response.write(contents);
+      response.end();
+    });
+  }else if(request.url ==='/images/cat1.jpg'){
+    fs.readFile('images/cat1.jpg', function(error, contents){
+      response.writeHead(200, {'Content-Type': 'image/*'});
+      response.write(contents);
+      response.end();
+    });
+  }else if(request.url ==='/images/cat2.jpg'){
+    fs.readFile('images/cat2.jpg', function(error, contents){
+      response.writeHead(200, {'Content-Type': 'image/*'});
+      response.write(contents);
+      response.end();
     });
   }else{
     response.writeHead(404);
